fe21f68f5e99bb63c1463a947f7bfaf2896a6781
Punam-Dev <punam.pal.dev@gmail.com>
2024-03-29 20:15:42 +0530

    Use 3.0

--- deploys/dev/backend.tf
+++ deploys/dev/backend.tf
@@ -4,8 +4,8 @@ terraform {
     key = "k8s-iam-role.tfstate"
     region = "us-east-1"
     encrypt = true
-    assume_role = {
-      role_arn = "arn:aws:iam::730335281548:role/k8s-iam-oidc-role"
-    }
+    # assume_role = {
+    #   role_arn = "arn:aws:iam::730335281548:role/k8s-iam-oidc-role"
+    # }
   }
 }
