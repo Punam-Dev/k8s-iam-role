32089c65cea4e410f749754bf04ff98402cbee51
Punam-Dev <punam.pal.dev@gmail.com>
2024-03-29 20:56:51 +0530

    Try to fix file path

--- deploys/dev/backend.tf
+++ deploys/dev/backend.tf
@@ -4,8 +4,5 @@ terraform {
     key = "k8s-iam-role.tfstate"
     region = "us-east-1"
     encrypt = true
-    # assume_role = {
-    #   role_arn = "arn:aws:iam::730335281548:role/k8s-iam-oidc-role"
-    # }
   }
 }
