c09a5640aed46955fe8433b50e2109f754043e8e
Punam-Dev <punam.pal.dev@gmail.com>
2024-03-29 20:16:30 +0530

    commented s3 backend

--- deploys/dev/backend.tf
+++ deploys/dev/backend.tf
@@ -1,11 +1,11 @@
-terraform {
-  backend "s3" {
-    bucket = "k8s-iam-role-dev"
-    key = "k8s-iam-role.tfstate"
-    region = "us-east-1"
-    encrypt = true
-    # assume_role = {
-    #   role_arn = "arn:aws:iam::730335281548:role/k8s-iam-oidc-role"
-    # }
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "k8s-iam-role-dev"
+#     key = "k8s-iam-role.tfstate"
+#     region = "us-east-1"
+#     encrypt = true
+#     # assume_role = {
+#     #   role_arn = "arn:aws:iam::730335281548:role/k8s-iam-oidc-role"
+#     # }
+#   }
+# }
